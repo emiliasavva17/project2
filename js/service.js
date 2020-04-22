d251ddfe7a85bfc842e0ddf06151385a53ba53ca
emiliasavva <emiliasavva.2000@gmail.com>
2020-04-22 22:28:54 +0200

    project2

---
+++ js/service.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+let rightar=document.getElementById("rightar");
+let leftar=document.getElementById("leftar");
+let b=document.getElementsByName("body");
+
+function right()
+{
+	document.body.style.background = "url('../pr2/img/marketing.png')";
+	
+}
+
+rightar.addEventListener("click",right);
+console.log('gujfgkjgkgkj');
